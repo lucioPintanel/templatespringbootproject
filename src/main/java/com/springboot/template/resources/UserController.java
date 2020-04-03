f458e90b9e79722fac0b726efb64c4fd9a326208
Lucio Pintanel <lm.pintanel@gmail.com>
2020-04-03 17:41:51 -0300

    - Adicionado o 'insert' do usuário novo ao banco de dados.

--- src/main/java/com/springboot/template/resources/UserController.java
+++ src/main/java/com/springboot/template/resources/UserController.java
@@ -1,20 +1,26 @@
 package com.springboot.template.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot.template.domain.User;
 import com.springboot.template.dto.UserDTO;
+import com.springboot.template.dto.UserNewDTO;
 import com.springboot.template.service.UserService;
 
 @RestController
@@ -37,6 +43,15 @@ public class UserController {
 		LOG.info(email);
 		User obj = this.userService.findByEmail(email);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody UserNewDTO objDto) {
+		User obj = this.userService.fromDTO(objDto);
+		obj = this.userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(method=RequestMethod.GET)
