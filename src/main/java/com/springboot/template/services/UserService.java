81234691b08545312657e09f28fef4a20c0259ca
Lucio Pintanel <lm.pintanel@gmail.com>
2020-04-06 22:36:01 -0300

    - Restrição de conteúdo: cliente só recupera ele mesmo.

--- src/main/java/com/springboot/template/services/UserService.java
+++ src/main/java/com/springboot/template/services/UserService.java
@@ -9,10 +9,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.springboot.template.security.UserSS;
 import com.springboot.template.domain.User;
+import com.springboot.template.domain.enums.Perfil;
 import com.springboot.template.dto.UserDTO;
 import com.springboot.template.dto.UserNewDTO;
 import com.springboot.template.repositories.UserRepository;
+import com.springboot.template.services.exceptions.AuthorizationException;
 import com.springboot.template.services.exceptions.DataIntegrityException;
 import com.springboot.template.services.exceptions.ObjectNotFoundException;
 
@@ -26,13 +29,23 @@ public class UserService {
 	private UserRepository userRepo;
 
 	public User find(Integer id) {
-		Optional<User> obj = userRepo.findById(id);
+		UserSS user = UserServiceSecurity.authenticated();
+		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		Optional<User> obj = this.userRepo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
 	}
 
 	public User findByEmail(String email) {
-		User obj = userRepo.findByEmail(email);
+		UserSS user = UserServiceSecurity.authenticated();
+		if (user==null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		User obj = this.userRepo.findByEmail(email);
 		if (obj == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + ", Tipo: " + User.class.getName());
 		}
@@ -40,13 +53,13 @@ public class UserService {
 	}
 
 	public List<User> findAll() {
-		return userRepo.findAll();
+		return this.userRepo.findAll();
 	}
 
 	@Transactional
 	public User insert(User obj) {
 		obj.setId(null);
-		obj = userRepo.save(obj);
+		obj = this.userRepo.save(obj);
 		return obj;
 	}
 
@@ -58,12 +71,13 @@ public class UserService {
 	public User update(User obj) {
 		User newObj = this.find(obj.getId());
 		this.updateData(newObj, obj);
-		return userRepo.save(newObj);
+		return this.userRepo.save(newObj);
 	}
 
 	public void delete(Integer id) {
+		this.find(id);
 		try {
-			userRepo.deleteById(id);
+			this.userRepo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
